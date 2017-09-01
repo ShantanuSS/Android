b4e8625b24e6a3d4cdf99e27b214fa487a06faf0
ShantanuSS <shantanusharma197@gmail.com>
2017-09-01 19:41:42 +0530

    Added AboutUs code

---
+++ FullList/app/src/main/java/ss/practice/AboutUs.java
@@ -0,0 +1,16 @@
+package ss.practice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by TOSHIBA on 31-08-2017.
+ */
+public class AboutUs extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
