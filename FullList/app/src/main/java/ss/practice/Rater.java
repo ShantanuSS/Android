cb442fed43395d388b9d9d9399febdecfe0dbce2
ShantanuSS <shantanusharma197@gmail.com>
2017-09-18 22:01:30 +0530

    setting up some variables

--- FullList/app/src/main/java/ss/practice/Rater.java
+++ FullList/app/src/main/java/ss/practice/Rater.java
@@ -4,4 +4,15 @@ package ss.practice;
  * Created by TOSHIBA on 18-09-2017.
  */
 public class Rater {
+    public static final String KEY_ROWID="_id";
+    public static final String KEY_NAME="persons_name";
+    public static final String KEY_RATINGS="ratings";
+
+
+    private static final String DATABASE_NAME="MYDB";
+    private static final String DATABASE_TABLE="peopleTable";
+    private static final String DATABASE_VERSION="1";
+    
+
+
 }
