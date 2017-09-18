4a7869ce433ac1088e4de9be742523447484d4bb
ShantanuSS <shantanusharma197@gmail.com>
2017-09-18 22:11:49 +0530

    Implementing SQLiteOpenHelper

--- FullList/app/src/main/java/ss/practice/Rater.java
+++ FullList/app/src/main/java/ss/practice/Rater.java
@@ -1,5 +1,9 @@
 package ss.practice;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by TOSHIBA on 18-09-2017.
  */
@@ -12,7 +16,27 @@ public class Rater {
     private static final String DATABASE_NAME="MYDB";
     private static final String DATABASE_TABLE="peopleTable";
     private static final String DATABASE_VERSION="1";
-    
 
+    private DBHelper ourHelper;//setting instance pf class
+    private final Context ourCon;
+    private SQLiteDatabase ourDB;
 
+    private static class DBHelper extends SQLiteOpenHelper{
+public DBHelper(Context context){
+    super(context,DATABASE_NAME,null, Integer.parseInt(DATABASE_VERSION));
+}
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
+    public Rater(Context c){
+        ourCon=c;
+    }
 }
